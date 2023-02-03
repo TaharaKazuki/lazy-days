beedf9c1b71095d1901687f02b334d9dbf8c4f66
Tahara Kazuki <ssp0727.lnc@gmail.com>
2023-02-04 07:31:26 +0900

    update

--- src/components/Header.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <ul class="nav nav-pills">
-      <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
-      <router-link to="/user" tag="li" active-class="active"><a>User</a></router-link>
-    </ul>
-  </div>
-</template>
